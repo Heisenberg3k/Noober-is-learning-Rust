e79e69a498f8ca5df12f4f5986311f19b5f8ebd5
Heisenberg3k <dmtai091@gmail.com>
2023-11-14 23:24:18 +0700

    test first commit

---
+++ hello_world/target/debug/deps/hello_world-ec66c4a08bda4161.d
@@ -0,0 +1,5 @@
+/home/heisenberg/Dev/Rust/Noober-is-learning-Rust/hello_world/target/debug/deps/hello_world-ec66c4a08bda4161: src/main.rs
+
+/home/heisenberg/Dev/Rust/Noober-is-learning-Rust/hello_world/target/debug/deps/hello_world-ec66c4a08bda4161.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/home/heisenberg/Dev/Rust/Noober-is-learning-Rust/hello_world/target/debug/hello_world: /home/heisenberg/Dev/Rust/Noober-is-learning-Rust/hello_world/src/main.rs
