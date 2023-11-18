36c04cd356df66aaf0f2fe38ec2172a7e964d700
Heisenberg3k <dmtai091@gmail.com>
2023-11-18 23:52:18 +0700

    update shadowing prime

---
+++ Ownership/prime/target/debug/deps/prime-cda744755bfe2b78.d
@@ -0,0 +1,5 @@
+/home/heisenberg/Dev/Rust/Noober-is-learning-Rust/Ownership/prime/target/debug/deps/prime-cda744755bfe2b78: src/main.rs
+
+/home/heisenberg/Dev/Rust/Noober-is-learning-Rust/Ownership/prime/target/debug/deps/prime-cda744755bfe2b78.d: src/main.rs
+
+src/main.rs:
